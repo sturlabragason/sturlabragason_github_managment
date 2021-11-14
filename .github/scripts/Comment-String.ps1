ab8ece81abbe9ff0c15039897f36c57da084795b
Sturla Bragason <sturla.bragason@gmail.com>
2021-11-14 09:58:13 +0000

    string

---
+++ .github/scripts/Comment-String.ps1
@@ -0,0 +1,15 @@
+function Comment-String {
+    param (
+        $String
+    )    
+    $StringLenght = $String.Length
+    $LineLenght = $StringLenght + 16
+    $Filler = "#" * $LineLenght
+    $CommentString = @()
+    $CommentString += " " + $Filler + "`n"
+    $CommentString += "####    " + $String + "    ####" + "`n"
+    $CommentString += $Filler
+    Write-Host $CommentString
+}
+
+Comment-String -String "sturlabragason"
