8250ddf3efb105c8e0cc7c8bd38166f0445da473
Sturla Bragason <sturla.bragason@gmail.com>
2021-12-11 08:50:05 +0000

    providername

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 4.0"
     }
     random = {
-      source  = "hashicorp/terraform-provider-random"
+      source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
   }
